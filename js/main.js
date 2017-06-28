9040aa4e6914c2b45cd239d52898277c2da9ecb2
Kohei <koh.yam.cntct@gmail.com>
2017-06-28 20:23:16 +0900

    js大改造
    article-detailのcss作成

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
-$(function(){
-    $('#btn div a').click(function(){
-    	$('#black').fadeIn('fast');
-    	$('#black section').hide();
-        $($(this).attr('href')).fadeToggle('fast');
-    });
-    $('#black').click(function(){
-    	$('#black').fadeOut();
-    });
+// home -> contents
+$('#btn div a').on("click touchend", function(){
+	$('#black').fadeIn('fast');
+	$('#black section').hide();
+    $($(this).attr('href')).fadeToggle('fast');
 });
+$(document).on('click touchend', function() {
+  if (!$(event.target).closest('.contents, #activity-list, #activity-detail, #btn div a').length) {
+    $('#black').fadeOut('fast');
+  }
+});
+$(function(){
+    $("#activity-list a").on("click touchend", function() {
+        $("#activity-detail article").hide();
+        $($(this).attr("href")).fadeToggle('fast');
+    });
+    return false;
+});
+
+
+//lazyload
