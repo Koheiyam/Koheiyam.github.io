ceae13d15b3130a27f58e9a3c8093da341dbe390
Kohei <koh.yam.cntct@gmail.com>
2017-09-17 16:01:33 +0900

    サイト置換

--- js/main.js
+++ js/main.js
@@ -1,52 +1,35 @@
-// home -> contents
-$('#top div a').on("click touchend", function(){
-	$('#container').fadeIn('fast');
-	$('#container section').hide();
-    $($(this).attr('href')).fadeToggle('fast');
-});
-$(document).on('click touchend', function() {
-  if (!$(event.target).closest('#top div a').length) {
-    /* $('#container').fadeOut('fast'); */
-  }
-});
 $(function(){
-    $("#activity-list a").on("click touchend", function() {
-        $("#activity-detail article").hide();
-        $($(this).attr("href")).fadeToggle('fast');
-    });
-    return false;
-});
-
-
-//pjax
-if ($.support.pjax) {
-  $(document).pjax('a:not([target]', '#pjax-container')
-}
-
-$(function(){
-        // pjaxの設定
+    // pjaxの設定
         $.pjax({
-            area : '.detailSns, #contents', //再描画させるエリア
+            area : '#pjax-container', //再描画させるエリア
             link : '.pjaxLink', // pjaxを発動させるlinkにつけるclass
             ajax : { timeout: 2500 }, // ajaxの通信のタイムアウト時間
             wait : 200, // 押してから発動するまでの時間
             cache: { click: false, submit: false, popstate: false },  // キャッシュの設定
             load: { head: 'base, meta, link', css: true, script: true },  // 再描画するエリアを選べる、インラインスクリプトを動作させるかどうか
-// http://falsandtru.github.io/jquery-pjax/api/callback/
 
             callbacks:{ // callbackを設定出来る
-                    ajax : {  //　
-                        success: function(event, setting, data, textStatus, jqXHR){
-                             console.log("ajax.success");
-                        }
-                    },
-                    update : {
-                        content: {
-                            after : function( event, setting, srcContent, dstContent ) {
-                                console.log(srcContent + ': update.content.after');
-                            }
+                ajax : {  //　
+                    success: function(event, setting, data, textStatus, jqXHR){
+                         console.log("ajax.success");
+                    }
+                },
+                update : {
+                    content: {
+                        after : function( event, setting, srcContent, dstContent ) {
+                            console.log(srcContent + ': update.content.after');
                         }
                     }
                 }
+            }
         });
+
+
+    // yam <-> sch
+
+
+    // list <-> detail
+
+
+    // detail -> anotherTop
 });
