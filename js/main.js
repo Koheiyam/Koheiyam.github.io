1c9f2c1b114520cb9ac5f5e131e5d00df6f5e870
Kohei <koh.yam.cntct@gmail.com>
2017-09-21 14:10:14 +0900

    Revert "pjax分岐"
    This reverts commit 7994221b50afac1be4e0c3d5ddfd657774873070.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 $(function(){
-    // yam <-> sch
-    $('#top a').on(click,function(){
+    // pjaxの設定
         $.pjax({
             area : '#pjax-container', //再描画させるエリア
             link : '.pjaxLink', // pjaxを発動させるlinkにつけるclass
@@ -24,35 +23,13 @@ $(function(){
                 }
             }
         });
-    });
+
+
+    // yam <-> sch
 
 
     // list <-> detail
-    $('.list a').on(click,function(){
-        $.pjax({
-            area : '#pjax-container-2', //再描画させるエリア
-            link : '.pjaxLink', // pjaxを発動させるlinkにつけるclass
-            ajax : { timeout: 2500 }, // ajaxの通信のタイムアウト時間
-            wait : 200, // 押してから発動するまでの時間
-            cache: { click: false, submit: false, popstate: false },  // キャッシュの設定
-            load: { head: 'base, meta, link', css: true, script: true },  // 再描画するエリアを選べる、インラインスクリプトを動作させるかどうか
 
-            callbacks:{ // callbackを設定出来る
-                ajax : {  //　
-                    success: function(event, setting, data, textStatus, jqXHR){
-                         console.log("ajax.success");
-                    }
-                },
-                update : {
-                    content: {
-                        after : function( event, setting, srcContent, dstContent ) {
-                            console.log(srcContent + ': update.content.after');
-                        }
-                    }
-                }
-            }
-        });
-    });
 
     // detail -> anotherTop
 });
