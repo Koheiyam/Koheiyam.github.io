0461fa1cf59ac971f90626826445696187256efc
Kohei <koh.yam.cntct@gmail.com>
2017-09-21 14:02:16 +0900

    セレクター修正

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function(){
     // yam <-> sch
-    $('#top a').on(click,function(){
+    $('#top').on(click,function(){
         $.pjax({
             area : '#pjax-container', //再描画させるエリア
             link : '.pjaxLink', // pjaxを発動させるlinkにつけるclass
@@ -28,7 +28,7 @@ $(function(){
 
 
     // list <-> detail
-    $('.list a').on(click,function(){
+    $('li').on(click,function(){
         $.pjax({
             area : '#pjax-container-2', //再描画させるエリア
             link : '.pjaxLink', // pjaxを発動させるlinkにつけるclass
