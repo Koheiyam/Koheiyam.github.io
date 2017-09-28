ccb8c6ecd7128f0132337ca2c31ce3820f259b87
Kohei <koh.yam.cntct@gmail.com>
2017-09-28 15:55:05 +0900

    jquery fadein-out

--- js/main.js
+++ js/main.js
@@ -1,26 +1,43 @@
-$(function(){
-    // pjaxの設定
-        $.pjax({
-            area : '#pjax-container', //再描画させるエリア
-            link : '.pjaxLink', // pjaxを発動させるlinkにつけるclass
-            ajax : { timeout: 2500 }, // ajaxの通信のタイムアウト時間
-            wait : 300, // 押してから発動するまでの時間
-            cache: { click: false, submit: false, popstate: false },  // キャッシュの設定
-            load: { head: 'base, meta, link', css: true, script: true },  // 再描画するエリアを選べる、インラインスクリプトを動作させるかどうか
+$('#pjax-container').hide();
+$('#pjax-container').fadeIn(1000);
+$('a').click(function(){
+    // URLを取得する
+    var url = $(this).attr('href');
 
-            callbacks:{ // callbackを設定出来る
-                ajax : {  //　
-                    success: function(event, setting, data, textStatus, jqXHR){
-                         console.log("ajax.success");
-                    }
-                },
-                update : {
-                    content: {
-                        after : function( event, setting, srcContent, dstContent ) {
-                            console.log(srcContent + ': update.content.after');
+    // URLが空ではない場合
+    if (url != '') {
+        // フェードアウトしてから、取得したURLにリンクする
+        $('#pjax-container').fadeOut(300);
+        setTimeout(function(){
+            location.href = url;
+        }, 300);
+    }
+    return false;
+
+});
+    $(function(){
+        // pjaxの設定
+            $.pjax({
+                area : '#pjax-container', //再描画させるエリア
+                link : '.pjaxLink', // pjaxを発動させるlinkにつけるclass
+                ajax : { timeout: 2500 }, // ajaxの通信のタイムアウト時間
+                wait : 300, // 押してから発動するまでの時間
+                cache: { click: false, submit: false, popstate: false },  // キャッシュの設定
+                load: { head: 'base, meta, link', css: true, script: true },  // 再描画するエリアを選べる、インラインスクリプトを動作させるかどうか
+
+                callbacks:{ // callbackを設定出来る
+                    ajax : {  //　
+                        success: function(event, setting, data, textStatus, jqXHR){
+                             console.log("ajax.success");
+                        }
+                    },
+                    update : {
+                        content: {
+                            after : function( event, setting, srcContent, dstContent ) {
+                                console.log(srcContent + ': update.content.after');
+                            }
                         }
                     }
                 }
-            }
-        });
-});
+            });
+    });
