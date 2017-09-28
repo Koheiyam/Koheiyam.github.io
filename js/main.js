5f734b3295e400b93437b13d33625cf1e2ecc311
Kohei <koh.yam.cntct@gmail.com>
2017-09-28 16:15:07 +0900

    function削除

--- js/main.js
+++ js/main.js
@@ -10,31 +10,29 @@ $('a').click(function(){
         $('#pjax-container').fadeOut(300);
         setTimeout(function(){
             location.href = url;
-            $(function(){
-                // pjaxの設定
-                    $.pjax({
-                        area : '#pjax-container', //再描画させるエリア
-                        link : '.pjaxLink', // pjaxを発動させるlinkにつけるclass
-                        ajax : { timeout: 2500 }, // ajaxの通信のタイムアウト時間
-                        wait : 300, // 押してから発動するまでの時間
-                        cache: { click: false, submit: false, popstate: false },  // キャッシュの設定
-                        load: { head: 'base, meta, link', css: true, script: true },  // 再描画するエリアを選べる、インラインスクリプトを動作させるかどうか
+            // pjaxの設定
+                $.pjax({
+                    area : '#pjax-container', //再描画させるエリア
+                    link : '.pjaxLink', // pjaxを発動させるlinkにつけるclass
+                    ajax : { timeout: 2500 }, // ajaxの通信のタイムアウト時間
+                    wait : 300, // 押してから発動するまでの時間
+                    cache: { click: false, submit: false, popstate: false },  // キャッシュの設定
+                    load: { head: 'base, meta, link', css: true, script: true },  // 再描画するエリアを選べる、インラインスクリプトを動作させるかどうか
 
-                        callbacks:{ // callbackを設定出来る
-                            ajax : {  //　
-                                success: function(event, setting, data, textStatus, jqXHR){
-                                     console.log("ajax.success");
-                                }
-                            },
-                            update : {
-                                content: {
-                                    after : function( event, setting, srcContent, dstContent ) {
-                                        console.log(srcContent + ': update.content.after');
-                                    }
+                    callbacks:{ // callbackを設定出来る
+                        ajax : {  //　
+                            success: function(event, setting, data, textStatus, jqXHR){
+                                 console.log("ajax.success");
+                            }
+                        },
+                        update : {
+                            content: {
+                                after : function( event, setting, srcContent, dstContent ) {
+                                    console.log(srcContent + ': update.content.after');
                                 }
                             }
                         }
-                    });
+                    }
             });
         }, 300);
     }
