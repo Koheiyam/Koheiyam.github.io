196b540f6026475bc13a65e65cb814b96bd12b91
Kohei <koh.yam.cntct@gmail.com>
2018-01-11 11:41:40 +0900

    アニメーション追加

--- js/main.js
+++ js/main.js
@@ -1,31 +1,12 @@
 $(function(){
-    // pjaxの設定
-        $.pjax({
-            area : '#pjax-container', //再描画させるエリア
-            link : '.pjaxLink', // pjaxを発動させるlinkにつけるclass
-            ajax : { timeout: 2500 }, // ajaxの通信のタイムアウト時間
-            wait : 300, // 押してから発動するまでの時間
-            cache: { click: false, submit: false, popstate: false },  // キャッシュの設定
-            load: { head: 'base, meta, link', css: true, script: true },  // 再描画するエリアを選べる、インラインスクリプトを動作させるかどうか
-
-            callbacks:{ // callbackを設定出来る
-                ajax : {  //　
-                    success: function(event, setting, data, textStatus, jqXHR){
-                         console.log("ajax.success");
-                    }
-                },
-                update : {
-                    content: {
-                        after : function( event, setting, srcContent, dstContent ) {
-                            console.log(srcContent + ': update.content.after');
-                        }
-                    }
-                }
+    $('a').click(function(){
+            var url = $(this).attr('href');
+            if (url != '') {
+                $('body').fadeOut(240);
+                setTimeout(function(){
+                    location.href = url;
+                }, 240);
             }
-        });
-
-    // fadeOut
-        $(document).bind('pjax:fetch', function() {
-            $('#pjax-container').fadeOut(300);
-        });
+            return false;
+    });
 });
